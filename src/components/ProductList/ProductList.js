dcc362fe631b05cf565eaff8bbdb3d4a7a3fa49b
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-03 13:10:17 +0530

    new commit

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import products from "../api/products.json"
+import BeforeCart from './CartButtons/BeforeCart'
+import AfterCart from './CartButtons/AfterCart'
+import "./ProductList.css"
+import {useSelector} from "react-redux"
+
 
 const ProductList = () => {
+  
+  const cart = useSelector((state)=> state.cart)
+
+  const [count,setCount] = useState(0)
+
+  const addToCart = ()=>{
+    setCount(1)
+  }
+
+
+
+
+
   return (
    <section className='container'>
      {products?.map((product,key)=>(
-      <div className='main-container' key={key}>
+      <div className='product-container' key={key}>
            <img src={product?.image} alt=''></img>
           <h3>{product?.title}</h3>
+          {count > 0 ?  <AfterCart/>: <BeforeCart addToCart={addToCart}/>}
+         
+        
       </div>
      ))}
    </section>
---
+++ src/components/redux/cart.js
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const INITIAL_STATE = {
+    cartList:[],
+    cartCount:0,
+}
+
+const cartSlice = createSlice({
+    name:"cart",
+    initialState:INITIAL_STATE,
+    reducers:{
+    addToCart : (state) => {
+      state.cartCount = 1
+    },
+    dncrement : (state) => {
+        state.cartCount -=1
+    },
+    increment : (state) => {
+        state.cartCount += 1
+    },
+    }
+})
+
+export const {addToCart,increment,dncrement} = cartSlice.actions
+export default cartSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1. Provider
+// 2. Store
+// 3. Reducer
+// 4. Action
+
+// Store = state
+// Action = Button Click
+// Reducer = State Update
+// Providet = Recat inject
+
